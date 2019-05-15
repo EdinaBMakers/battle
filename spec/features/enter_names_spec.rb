fdab7ee71c0c943bfa5758bb1df482013e52f519
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-15 19:40:18 +0100

    Extract common steps to helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Name entry' do
   scenario "players' names are displayed after submission" do
-    visit '/'
-    fill_in 'player_1_name', with: 'Alice'
-    fill_in 'player_2_name', with: 'Bob'
-    click_button 'Submit'
+    sign_in_and_play
 
     expect(page).to have_content('Alice vs. Bob')
   end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,7 @@
 feature 'View hit points' do
   scenario "after submitting players' names player 2's hit points are displayed" do
-    visit '/'
-    fill_in 'player_1_name', with: 'Alice'
-    fill_in 'player_2_name', with: 'Bob'
-    click_button 'Submit'
-
+    sign_in_and_play
+    
     expect(page).to have_content('Bob: 60HP')
   end
 end
