741351ea64f684c47459bd2a0812c477e0b69916
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-16 20:56:30 +0100

    Store players in Game

--- app.rb
+++ app.rb
@@ -9,30 +9,25 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
 
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    @title = "#{@player_1.name} vs. #{@player_2.name}"
+    @title = "#{$game.player_1.name} vs. #{$game.player_2.name}"
     erb(:play)
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    $game.attack($game.player_2)
 
     redirect '/attack_result'
   end
 
   get '/attack_result' do
-    @player_1 = $player_1
-    @player_2 = $player_2
     erb(:attack_result)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,26 @@
 require 'game'
 
 describe Game do
-  let(:subject) { Game.new }
-  let(:player) { double :player }
+  let(:subject) { Game.new(player_1, player_2) }
+  let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2 }
+
+  describe '#player_1' do
+    it 'returns player 1' do
+      expect(subject.player_1).to eq(player_1)
+    end
+  end
+
+  describe '#player_2' do
+    it 'returns player 2' do
+      expect(subject.player_2).to eq(player_2)
+    end
+  end
 
   describe '#attack' do
     it 'damages the attacked player' do
-      expect(player).to receive(:receive_damage)
-      subject.attack(player)
+      expect(player_2).to receive(:receive_damage)
+      subject.attack(player_2)
     end
   end
 end
